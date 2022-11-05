c04650611c348adaa738ac135383e022f0673ab4
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-05 19:31:26 -0300

    :bricks: Preparing sample tracks

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,71 @@
+import { Container, Navbar, Nav, CardGroup } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Footer from "../../components/Footer/Footer";
 import "./styles.css";
 
 function Home() {
   return (
     <>
-      <div className="navBar">
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Row className="apresentation">
+        <Col className="logoOrange">
+          <img
+            src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1639661491/$s20z34nynz"
+            alt="Logo Orange"
+          />
+        </Col>
+        <Col className="textApresentation">
+          <h1>A comunidade tech mais vitaminada!</h1>
+          <h3>
+            Conecte-se com pessoas com os mesmos interesses que você enquanto
+            aprende sobre as maiores tendências do mundo da tecnologia 🍊
+          </h3>
+        </Col>
+      </Row>
+      {/* <CardGroup className="examplesTrails">
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1662729347/$mip3cw6frz9"
+          />
+          <Card.Body>
+            <Card.Text>Desenvolvimento Full Stack</Card.Text>
+          </Card.Body>
+          <Card.Img
+            variant="top"
+            src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1662729347/$mip3cw6frz9"
+          />
+          <Card.Body>
+            <Card.Text>Desenvolvimento Full Stack</Card.Text>
+          </Card.Body>
+          <Card.Img
+            variant="top"
+            src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1662729347/$mip3cw6frz9"
+          />
+          <Card.Body>
+            <Card.Text>Desenvolvimento Full Stack</Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup> */}
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
+
+{
+  /* <div className="navBar">
         <img
           src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1654526197/$g74vm2w5o56"
           alt=""
@@ -14,8 +75,11 @@ function Home() {
           <ul>Trilhas</ul>
           <ul>Login</ul>
         </li>
-      </div>
-      <div className="apresentation">
+      </div> */
+}
+
+{
+  /* <div className="apresentation">
         <div className="logoOrange">
           <img
             src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1639661491/$s20z34nynz"
@@ -23,17 +87,15 @@ function Home() {
           />
         </div>
         <div className="textApresentation">
+          <h1>A comunidade tech mais vitaminada!</h1>
           <h3>
-            Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun Lorem
-            ipsun Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-            Lorem ipsun Lorem ipsun{" "}
+            Conecte-se com pessoas com os mesmos interesses que você enquanto
+            aprende sobre as maiores tendências do mundo da tecnologia 🍊
           </h3>
         </div>
-      </div>
-      <div className="examplesTrails">Exemplo de Trilhas</div>
-      <Footer />
-    </>
-  );
+      </div> */
 }
 
-export default Home;
+{
+  /* <div className="examplesTrails">Exemplo de Trilhas</div> */
+}
