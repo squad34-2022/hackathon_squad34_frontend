9730cf9297e9c586ecaae2870465369ee1f92eb7
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-05 21:27:41 -0300

    :recycle:: Refactoring footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,61 @@
 import "./styles.css";
+import { Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <>
-      <div className="footer">
-        <div>
-          <img
-            src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1654865437/$khtwl6dbna"
-            alt=""
-          />
-        </div>
-        <div className="items-footer">
-          <li>
-            <ul>Home</ul>
-            <ul>Trilhas</ul>
-            <ul>Comunidade</ul>
-            <ul>Contato</ul>
-          </li>
-        </div>
-        <div className="localizations">
-          <li>
-            <ul>
-              <p>Onde Estamos</p>
-            </ul>
-            <ul>
-              <div>
-                <p>São Paulo</p>
-                <p>MATRIZ</p>
-              </div>
-            </ul>
-            <ul>
-              <p>Rua Bela Cintra, 986 - 2º andar Consolação, São Paulo - SP</p>
-            </ul>
-            <ul>
-              <div>
-                <p>Santos</p>
-                <p>FILIAL</p>
-              </div>
-            </ul>
-            <ul>
-              <p>Praça dos Expedicionários, 19 Sala 22 Gonzaga, Santos - SP</p>
-            </ul>
-          </li>
-        </div>
-      </div>
-    </>
+    <div className="footer">
+      <Row className="justify-content-md-center footer-container">
+        <Col xs lg="5">
+          <div>
+            <img
+              src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1654865437/$khtwl6dbna"
+              alt=""
+            />
+          </div>
+        </Col>
+        <Col lg="5">
+          <div className="items-footer">
+            <li>
+              <ul>Home</ul>
+              <ul>Trilhas</ul>
+              <ul>Comunidade</ul>
+              <ul>Contato</ul>
+            </li>
+          </div>
+        </Col>
+        <Col xs lg="5">
+          <div className="localizations">
+            <li>
+              <ul>
+                <p>Onde Estamos</p>
+              </ul>
+              <ul>
+                <div>
+                  <p>São Paulo</p>
+                  <p>MATRIZ</p>
+                </div>
+              </ul>
+              <ul>
+                <p>
+                  Rua Bela Cintra, 986 - 2º andar Consolação, São Paulo - SP
+                </p>
+              </ul>
+              <ul>
+                <div>
+                  <p>Santos</p>
+                  <p>FILIAL</p>
+                </div>
+              </ul>
+              <ul>
+                <p>
+                  Praça dos Expedicionários, 19 Sala 22 Gonzaga, Santos - SP
+                </p>
+              </ul>
+            </li>
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
