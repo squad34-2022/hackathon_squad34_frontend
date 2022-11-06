02301c56f322130e4179bd83e005eb2d298dfe85
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-06 15:28:22 -0300

    :recycle:: refactor register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -6,14 +6,14 @@ export default function Register() {
   return (
     <Container fluid className="g-0 ">
       <Row>
-        <Col className="col-lg-6">
+        <Col className="col-lg-6 image-container">
           <Image
             src={background}
             className="image-background"
             alt="background-register"
           />
         </Col>
-        <Col className="d-flex justify-content-center align-items-center col-lg-6">
+        <Col className="d-flex justify-content-center align-items-center col-lg-6 register-container">
           <FormRegister />
         </Col>
       </Row>
