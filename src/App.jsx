dcf62960b4b3507b245c35121b9fb811652a9a82
Kaique Lira <Kaiqueliira@gmail.com>
2022-11-06 16:50:25 -0300

    :heavy_plus_sign: Added dependencies of mui icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
