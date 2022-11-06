6d772ab5676141c12c846f1c9856c338b1501cab
Kaique Lira <Kaiqueliira@gmail.com>
2022-11-05 23:26:36 -0300

    :sparkles: feat: Created Application Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
