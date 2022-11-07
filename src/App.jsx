12d1c49dae96821f16f867dd1fc69c58b888d7c0
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-07 01:22:38 -0300

    :sparkles::Starting Dashboard Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cadastro" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
