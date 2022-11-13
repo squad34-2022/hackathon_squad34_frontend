00904f3c40976acb509ce47b5fb898a6728f4b15
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-13 01:03:57 -0300

    :sparkles: feat: Dynamic data integration on the courses page

--- src/pages/Courses/Course.jsx
+++ src/pages/Courses/Course.jsx
@@ -3,75 +3,28 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
+import { useEffect, useState } from "react";
 import AccordionCourses from "../../components/AccordionCourses";
 import Navbar from "../../components/Navbar/Navbar";
+import CourseServices from "../../services/coursesServices";
+import TrailServices from "../../services/trailServices";
 
 const drawerWidth = 240;
 
-const teste = [
-  {
-    title: "Dev Full Stack",
-    _id: "1",
-    courses: [
-      {
-        _id: "1",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://www.youtube.com/embed/pdLZ7KvTXTE",
-      },
-      {
-        _id: "2",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://blog.fcamara.com.br/squads-o-que-e-e-como-funciona/",
-      },
-    ],
-  },
-  {
-    title: "UX",
-    _id: "2",
-    courses: [
-      {
-        _id: "1",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://www.alura.com.br/artigos/como-se-tornar-um-desenvolvedor-full-stack-no-proximo-ano",
-      },
-      {
-        _id: "2",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://www.youtube.com/embed/n0KH8dQSrv0",
-      },
-    ],
-  },
-  {
-    title: "QA",
-    _id: "3",
-    courses: [
-      {
-        _id: "1",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://blog.fcamara.com.br/squads-o-que-e-e-como-funciona/",
-      },
-      {
-        _id: "2",
-        title: "JS",
-        author: "FCamara",
-        type: "Artigo",
-        link: "https://www.youtube.com/embed/eVwTlOuzT0Q",
-      },
-    ],
-  },
-];
-
 function Courses() {
+  const [trails, setTrails] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    TrailServices.getAll()
+      .then(({ data }) => setTrails(data))
+      .catch((error) => console.log(error));
+
+    CourseServices.getAll()
+      .then(({ data }) => setCourses(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -94,12 +47,12 @@ function Courses() {
       >
         <Toolbar />
         <Box sx={{ overflow: "auto" }}>
-          {teste?.map(({ title, _id, courses }) => (
+          {trails?.map(({ _id, title, courses }) => (
             <AccordionCourses key={_id} title={title} courses={courses} />
           ))}
         </Box>
       </Drawer>
-      <Box ml={25} component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box ml={5} container component="main" sx={{ height: "100vh" }}>
         <Toolbar />
         <iframe
           name="aula1"
