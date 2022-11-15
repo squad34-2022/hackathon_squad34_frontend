971fdd60c04012fc2902d2a89e76c9c1628071db
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-14 22:45:05 -0300

    :recycle: feat: Adjusting items in accordion on admin page

--- src/components/AccordionDetailsAdminDash/AccordionDetailsCourses.jsx
+++ src/components/AccordionDetailsAdminDash/AccordionDetailsCourses.jsx
@@ -1,14 +1,14 @@
-import { AccordionDetails, Box, Button, Chip, Typography } from "@mui/material";
+import { AccordionDetails, Chip, Grid, Typography } from "@mui/material";
 
 function AccordionDetailsCourses({ trail, title, author }) {
   return (
     <AccordionDetails>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Grid columns={3} container justifyContent="space-between" width={750} alignItems="center">
         <Chip label={trail?.title} />
         <Typography textAlign="center">{title}</Typography>
         <Typography textAlign="center">{author}</Typography>
 
-      </Box>
+      </Grid>
     </AccordionDetails>
   );
 }
